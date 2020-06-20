b6961af9b76a2e0e05f156200f41a4ee43d24866
Rolf Eike Beer <eike@sf-mail.de>
2020-06-20 09:45:03 +0200

    reintroduce alloc_re()
    This is used by the large-dns patch, and may be used by others. Add it back in
    a way that it will not be compiled in our normal build, but a simple compiler
    define allows external patches to find the needed environment.

--- alloc.h
+++ alloc.h
@@ -6,4 +6,19 @@
 #define alloc(x) malloc(x)
 #define alloc_free(x) free(x)
 
+/*
+ * This functions should not be used in any new code. It is provided so that
+ * external patches that rely on it's presence still work correctly.
+ */
+#ifndef DONT_PROVIDE_OBSOLETE
+static inline int alloc_re(void **x, unsigned int m, unsigned int n)
+{
+  void *y = realloc(*x, n);
+  (void)m;
+  if (y != NULL)
+    *x = y;
+  return !!y;
+}
+#endif
+
 #endif
